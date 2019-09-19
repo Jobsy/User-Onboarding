3d82a19335b275d3e02ba8d350d5f48f63b039e9
Oluwajoba <oluwajobabello@gmail.com>
2019-09-19 15:15:05 +0100

    convert regular form to formik form / remove useState and onChange event / step-1

--- user-onboarding/src/components/Container.js
+++ user-onboarding/src/components/Container.js
@@ -3,31 +3,33 @@ import { withFormik, Form, Field } from "formik";
 
 function LoginForm() {
  
-  const [user, setUser] = useState({ name: "", email: "", password: "", tosAccept: "", tosDecline: "" });
+  // const [user, setUser] = useState({ name: "", email: "", password: "", tosAccept: "", tosDecline: "" });
 
-  const handleChange = event => {
-    setUser({ ...user, [event.target.name]: event.target.value });
-  };
+  // const handleChange = event => {
+  //   setUser({ ...user, [event.target.name]: event.target.value });
+  // };
 
-  const handleSubmit = event => {
-    event.preventDefault();
-    console.log(user.name);
-    console.log(user.email);
-    console.log(user.password);
-    console.log(user.tosAccept);
-    console.log(user.tosDecline);
-  };
+  // const handleSubmit = event => {
+  //   event.preventDefault();
+  //   console.log(user.name);
+  //   console.log(user.email);
+  //   console.log(user.password);
+  //   console.log(user.tosAccept);
+  //   console.log(user.tosDecline);
+  // };
 
   return (
-    <div className="loginForm">
-      {console.log(user)}
-      <form onSubmit={event => handleSubmit(event)}>
+    
+    // <div className="loginForm">
+    //   {console.log(user)}
+      // <form onSubmit={event => handleSubmit(event)}>
+      <Form>
         <label>
           Name:
           <input
             type="text"
             name="name"
-            onChange={event => handleChange(event)}
+            // onChange={event => handleChange(event)}
           />
         </label>
         <br />
@@ -36,7 +38,7 @@ function LoginForm() {
           <input
             type="email"
             name="email"
-            onChange={event => handleChange(event)}
+            // onChange={event => handleChange(event)}
           />
         </label>
         <br />
@@ -45,7 +47,7 @@ function LoginForm() {
           <input
             type="text"
             name="password"
-            onChange={event => handleChange(event)}
+            // onChange={event => handleChange(event)}
           />
         </label>
         <br />
@@ -55,19 +57,19 @@ function LoginForm() {
           <input
             type="checkbox"
             name="tosAccept"
-            onChange={event => handleChange(event)}
+            // onChange={event => handleChange(event)}
           />     Accept
           <br />
           <input
             type="checkbox"
             name="tosDecline"
-            onChange={event => handleChange(event)}
+            // onChange={event => handleChange(event)}
           />       Decline
         </label>
         <br />
         <button>Submit!</button>
-      </form>
-    </div>
+      </Form>
+    // </div>
   );
 }
 
