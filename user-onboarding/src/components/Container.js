787d9782a75f69b0a8fb0fd5adf4e737cade1089
Oluwajoba <oluwajobabello@gmail.com>
2019-09-19 17:52:39 +0100

    add 'touched' and 'errors' validation for checkbox / step-2

--- user-onboarding/src/components/Container.js
+++ user-onboarding/src/components/Container.js
@@ -4,8 +4,8 @@ import { withFormik, Form, Field } from "formik";
 import * as Yup from "yup";
 
 
-function LoginForm({errors, touched}) {
- 
+function LoginForm({ errors, touched, values }) {
+
   // const [user, setUser] = useState({ name: "", email: "", password: "", tosAccept: "", tosDecline: "" });
 
   // const handleChange = event => {
@@ -22,89 +22,94 @@ function LoginForm({errors, touched}) {
   // };
 
   return (
-    
+
     // <div className="loginForm">
     //   {console.log(user)}
-      // <form onSubmit={event => handleSubmit(event)}>
-      <Form>
-        <label>
-          Name:
+    // <form onSubmit={event => handleSubmit(event)}>
+    <Form>
+      <label>
+        Name:
                     <Field
-            type="text"
-            name="name"
-            // onChange={event => handleChange(event)}
-          />
-          {touched.name && errors.name && <p>{errors.name}</p>}
+          type="text"
+          name="name"
+        // onChange={event => handleChange(event)}
+        />
+        {touched.name && errors.name && <p>{errors.name}</p>}
 
-        </label>
-        <br />
-        <label>
-          Email:
+      </label>
+      <br />
+      <label>
+        Email:
           <Field
-            type="email"
-            name="email"
-            // onChange={event => handleChange(event)}
-          />
-          {touched.email && errors.email && <p>{errors.email}</p>}
-        </label>
-        <br />
-        <label>
-          Password:
+          type="email"
+          name="email"
+        // onChange={event => handleChange(event)}
+        />
+        {touched.email && errors.email && <p>{errors.email}</p>}
+      </label>
+      <br />
+      <label>
+        Password:
           <Field
-            type="password"
-            name="password"
-            // onChange={event => handleChange(event)}
-          />
-          {touched.password &&errors.password && <p>{errors.password}</p>}
-        </label>
-        <br />
-        <label>
-          Terms of Service:
+          type="password"
+          name="password"
+        // onChange={event => handleChange(event)}
+        />
+        {touched.password && errors.password && <p>{errors.password}</p>}
+      </label>
+      <br />
+      <label>
+        Terms of Service:
           <br />
-          <Field
-            type="checkbox"
-            name="tosAccept"
-            // onChange={event => handleChange(event)}
-          />     Accept
+        <Field
+          type="checkbox"
+          name="tosAccept"
+          checked={values.tosAccept}
+        // onChange={event => handleChange(event)}
+        />     Accept
           <br />
-          <Field
+        {/* <Field
             type="checkbox"
             name="tosDecline"
             // onChange={event => handleChange(event)}
-          />       Decline
-        </label>
-        <br />
-        <button>Submit!</button>
-      </Form>
+          />       Decline */}
+          {touched.tosAccept && errors.tosAccept && <p>{errors.tosAccept}</p>}
+      </label>
+      <br />
+      <button>Submit!</button>
+    </Form>
     // </div>
   );
 }
 
 const FormikLoginForm = withFormik({
 
-  mapPropsToValues({ name, email, password }) {
+  mapPropsToValues({ name, email, password, tosAccept }) {
     return {
       name: name || "",
       email: email || "",
-      password: password || ""
+      password: password || "",
+      tosAccept: tosAccept || false
     };
   },
 
   validationSchema: Yup.object().shape({
     name: Yup.string()
-    .min(3)
-    .required(),
+      .min(3)
+      .required(),
     email: Yup.string()
       .email()
       .required(),
     password: Yup.string()
       .min(6)
-      .required()
+      .required(),
+    tosAccept: Yup.boolean()
+    .oneOf([true], "Must accept Terms of Service to submit"),
   }),
 
 
 
-  })(LoginForm);
+})(LoginForm);
 
 // export default LoginForm;
 export default FormikLoginForm;
