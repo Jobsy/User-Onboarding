7590a8d0593d0e9348491cb559cc2ccc946a3a05
Oluwajoba <oluwajobabello@gmail.com>
2019-09-20 03:42:22 +0100

    render users with '.map' / Display.js / step-4

--- user-onboarding/src/App.js
+++ user-onboarding/src/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import ReactDom from "react-dom";
 import Container from "./components/Container";
 
-
 ReactDom.render(
   <Container />,
   document.querySelector("#root"),
