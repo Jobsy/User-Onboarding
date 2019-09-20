cfbb8a3c5f4c7e662ace18e495d62b171cd7df88
Oluwajoba <oluwajobabello@gmail.com>
2019-09-20 04:19:22 +0100

    create separate component for Form / Form.js / step-4

---
+++ user-onboarding/src/components/Form.js
@@ -0,0 +1,60 @@
+// import React, { useState, useEffect } from "react";
+// import { withFormik, Form, Field } from "formik";
+import { withFormik } from "formik";
+import * as Yup from "yup";
+import axios from "axios"
+
+import LoginForm from "./Container"
+
+const FormikLoginForm = withFormik({
+
+    mapPropsToValues({ name, email, password, tos }) {
+      return {
+        name: name || "",
+        email: email || "",
+        password: password || "",
+        tos: tos || false
+      };
+    },
+  
+    validationSchema: Yup.object().shape({
+      name: Yup.string()
+        .min(3)
+        .required(),
+      email: Yup.string()
+        .email()
+        .required(),
+      password: Yup.string()
+        .min(6)
+        .required(),
+      tos: Yup.boolean()
+        .oneOf([true], "Must accept Terms of Service to submit"),
+    }),
+  
+    handleSubmit(values, { resetForm, setStatus }) {
+  
+      const sentData = {
+        // data: {
+        name: values.name,
+        email: values.email,
+        password: values.password,
+        tos: values.tos,
+        // display: valu[]
+        // }
+      };
+  
+      axios.post(" https://reqres.in/api/users", sentData)
+        .then(response => {
+          setStatus(response.data);
+          resetForm();
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    }
+  
+  })(LoginForm);
+  
+  
+  export default FormikLoginForm;
+  
