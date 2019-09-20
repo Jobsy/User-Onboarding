d97bfcb8a31331dcd1ac43ab3a6e2e496079f326
Oluwajoba <oluwajobabello@gmail.com>
2019-09-20 03:43:59 +0100

    code clean-up and indentation / Display.js / step-4

--- user-onboarding/src/components/Display.js
+++ user-onboarding/src/components/Display.js
@@ -1,24 +1,19 @@
 import React from 'react';
 
 
-
-
-
 const Display = props => {
-console.log("/////: ", props.users)
-    return (
-      <div>
-        <h3>Posted Users</h3>
-        {props.users.map(user => (
-          <>{user.name} <br/>  
-          {user.email} <br/> 
-          {user.password} <br/> 
-          
-          </>
-        ))}
-      </div>
-    )
-  };
-  
-  export default Display;
-  
+
+  return (
+    <div>
+      <h3>Posted Users</h3>
+      {props.users.map(user => (
+        <>{user.name} <br />
+          {user.email} <br />
+          {user.password} <br />
+        </>
+      ))}
+    </div>
+  )
+};
+
+export default Display;
